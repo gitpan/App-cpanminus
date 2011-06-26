cb26f886fe2a9829bb404f5b2dc30517b7f51931
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-06-26 17:59:17 +0000

    Import of MIYAGAWA/App-cpanminus-1.49_01 from CPAN.
    gitpan-cpan-distribution: App-cpanminus
    gitpan-cpan-version:      1.49_01
    gitpan-cpan-path:         MIYAGAWA/App-cpanminus-1.49_01.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     developer

--- lib/App/cpanminus.pm
+++ lib/App/cpanminus.pm
@@ -1,5 +1,6 @@
 package App::cpanminus;
-our $VERSION = "1.4008";
+our $VERSION = "1.49_01";
+$VERSION = eval $VERSION;
 
 =head1 NAME
 
@@ -188,7 +189,7 @@ Building RPM packages from CPAN modules
 
 =item *
 
-Listing the outdated modules that needs upgrading. See L<cpan-outdated>
+Listing the outdated modules that needs upgrading. See L<App::cpanoutdated>
 
 =item *
 
@@ -226,6 +227,20 @@ The standalone executable contains the following modules embedded.
 
 =item L<version> Copyright 2004-2010 John Peacock
 
+=item L<JSON::PP> Copyright 2007−2011 by Makamaka Hannyaharamitu
+
+=item L<CPAN::Meta> Copyright (c) 2010 by David Golden and Ricardo Signes
+
+=item L<Try::Tiny> Copyright (c) 2009 Yuval Kogman
+
+=item L<parent> Copyright (c) 2007-10 Max Maischein
+
+=item L<Version::Requirements> copyright (c) 2010 by Ricardo Signes
+
+=item L<Dist::Metadata> copyright (c) 2011 by Randy Stauner
+
+=item L<CPAN::Meta::YAML> copyright (c) 2010 by Adam Kennedy
+
 =back
 
 =head1 LICENSE
@@ -239,8 +254,8 @@ Same as Perl.
 Patches and code improvements were contributed by:
 
 Goro Fuji, Kazuhiro Osawa, Tokuhiro Matsuno, Kenichi Ishigaki, Ian
-Wells, Pedro Melo, Masayoshi Sekimura, Matt S Trout, squeeky, horus
-and Ingy dot Net.
+Wells, Pedro Melo, Masayoshi Sekimura, Matt S Trout (mst), squeeky,
+horus and Ingy dot Net.
 
 =head2 ACKNOWLEDGEMENTS
 
