5203f4cb7b5ca04a7d3d0247c54965be9e4be122
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-07-25 16:58:24 +0000

    Import of MIYAGAWA/App-cpanminus-1.6933 from CPAN.
    gitpan-cpan-distribution: App-cpanminus
    gitpan-cpan-version:      1.6933
    gitpan-cpan-path:         MIYAGAWA/App-cpanminus-1.6933.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- lib/App/cpanminus/fatscript.pm
+++ lib/App/cpanminus/fatscript.pm
@@ -20,7 +20,7 @@ my %fatpacked;
 
 $fatpacked{"App/cpanminus.pm"} = <<'APP_CPANMINUS';
   package App::cpanminus;
-  our $VERSION = "1.6932";
+  our $VERSION = "1.6933";
   
   =encoding utf8
   
@@ -307,6 +307,139 @@ $fatpacked{"App/cpanminus.pm"} = <<'APP_CPANMINUS';
   1;
 APP_CPANMINUS
 
+$fatpacked{"App/cpanminus/CPANVersion.pm"} = <<'APP_CPANMINUS_CPANVERSION';
+  package App::cpanminus::CPANVersion;
+  # copy of CPAN::Version since it's not core on older 5.8
+  
+  use strict;
+  #use vars qw($VERSION);
+  #$VERSION = "5.5001";
+  
+  # CPAN::Version::vcmp courtesy Jost Krieger
+  sub vcmp {
+      my($self,$l,$r) = @_;
+      local($^W) = 0;
+      #CPAN->debug("l[$l] r[$r]") if $CPAN::DEBUG;
+  
+      return 0 if $l eq $r; # short circuit for quicker success
+  
+      for ($l,$r) {
+          s/_//g;
+      }
+      #CPAN->debug("l[$l] r[$r]") if $CPAN::DEBUG;
+      for ($l,$r) {
+          next unless tr/.// > 1 || /^v/;
+          s/^v?/v/;
+          1 while s/\.0+(\d)/.$1/; # remove leading zeroes per group
+      }
+      #CPAN->debug("l[$l] r[$r]") if $CPAN::DEBUG;
+      if ($l=~/^v/ <=> $r=~/^v/) {
+          for ($l,$r) {
+              next if /^v/;
+              $_ = $self->float2vv($_);
+          }
+      }
+      #CPAN->debug("l[$l] r[$r]") if $CPAN::DEBUG;
+      my $lvstring = "v0";
+      my $rvstring = "v0";
+      if ($] >= 5.006
+       && $l =~ /^v/
+       && $r =~ /^v/) {
+          $lvstring = $self->vstring($l);
+          $rvstring = $self->vstring($r);
+          #CPAN->debug(sprintf "lv[%vd] rv[%vd]", $lvstring, $rvstring) if $CPAN::DEBUG;
+      }
+  
+      return (
+              ($l ne "undef") <=> ($r ne "undef")
+              ||
+              $lvstring cmp $rvstring
+              ||
+              $l <=> $r
+              ||
+              $l cmp $r
+      );
+  }
+  
+  sub vgt {
+      my($self,$l,$r) = @_;
+      $self->vcmp($l,$r) > 0;
+  }
+  
+  sub vlt {
+      my($self,$l,$r) = @_;
+      $self->vcmp($l,$r) < 0;
+  }
+  
+  sub vge {
+      my($self,$l,$r) = @_;
+      $self->vcmp($l,$r) >= 0;
+  }
+  
+  sub vle {
+      my($self,$l,$r) = @_;
+      $self->vcmp($l,$r) <= 0;
+  }
+  
+  sub vstring {
+      my($self,$n) = @_;
+      $n =~ s/^v// or die "CPAN::Version::vstring() called with invalid arg [$n]";
+      pack "U*", split /\./, $n;
+  }
+  
+  # vv => visible vstring
+  sub float2vv {
+      my($self,$n) = @_;
+      my($rev) = int($n);
+      $rev ||= 0;
+      my($mantissa) = $n =~ /\.(\d{1,12})/; # limit to 12 digits to limit
+                                            # architecture influence
+      $mantissa ||= 0;
+      $mantissa .= "0" while length($mantissa)%3;
+      my $ret = "v" . $rev;
+      while ($mantissa) {
+          $mantissa =~ s/(\d{1,3})// or
+              die "Panic: length>0 but not a digit? mantissa[$mantissa]";
+          $ret .= ".".int($1);
+      }
+      # warn "n[$n]ret[$ret]";
+      $ret =~ s/(\.0)+/.0/; # v1.0.0 => v1.0
+      $ret;
+  }
+  
+  sub readable {
+      my($self,$n) = @_;
+      $n =~ /^([\w\-\+\.]+)/;
+  
+      return $1 if defined $1 && length($1)>0;
+      # if the first user reaches version v43, he will be treated as "+".
+      # We'll have to decide about a new rule here then, depending on what
+      # will be the prevailing versioning behavior then.
+  
+      if ($] < 5.006) { # or whenever v-strings were introduced
+          # we get them wrong anyway, whatever we do, because 5.005 will
+          # have already interpreted 0.2.4 to be "0.24". So even if he
+          # indexer sends us something like "v0.2.4" we compare wrongly.
+  
+          # And if they say v1.2, then the old perl takes it as "v12"
+  
+  #        if (defined $CPAN::Frontend) {
+  #            $CPAN::Frontend->mywarn("Suspicious version string seen [$n]\n");
+  #        } else {
+              warn("Suspicious version string seen [$n]\n");
+  #        }
+          return $n;
+      }
+      my $better = sprintf "v%vd", $n;
+      #CPAN->debug("n[$n] better[$better]") if $CPAN::DEBUG;
+      return $better;
+  }
+  
+  1;
+  
+  __END__
+APP_CPANMINUS_CPANVERSION
+
 $fatpacked{"App/cpanminus/Dependency.pm"} = <<'APP_CPANMINUS_DEPENDENCY';
   package App::cpanminus::Dependency;
   use strict;
@@ -369,11 +502,12 @@ $fatpacked{"App/cpanminus/ParsePM.pm"} = <<'APP_CPANMINUS_PARSEPM';
   use Safe;
   use JSON::PP;
   use Dumpvalue;
-  use CPAN::Version;
+  #use CPAN::Version;
   use version ();
   use File::Spec ();
   use File::Temp ();
   use POSIX ':sys_wait_h';
+  use App::cpanminus::CPANVersion;
   
   our $VERSION = '0.04';
   our $VERBOSE = 0;
@@ -841,7 +975,7 @@ $fatpacked{"App/cpanminus/ParsePM.pm"} = <<'APP_CPANMINUS_PARSEPM';
   # from PAUSE::pmfile;
   sub _force_numeric {
       my($self,$v) = @_;
-      $v = CPAN::Version->readable($v);
+      $v = App::cpanminus::CPANVersion->readable($v);
   
       if (
           $v =~
@@ -3000,15 +3134,12 @@ $fatpacked{"App/cpanminus/script.pm"} = <<'APP_CPANMINUS_SCRIPT';
       $self->diag_progress("Configuring $target");
   
       my $configure_state = $self->configure_this($dist, $depth);
-  
       $self->diag_ok($configure_state->{configured_ok} ? "OK" : "N/A");
   
-      my $root_target = (($self->{installdeps} or $self->{showdeps}) and $depth == 0);
-  
-      # Do not scan packages for apps, especially with Carton and local libs in them
-      $dist->{provides} = $self->extract_packages($dist->{meta}, ".") unless $root_target;
+      $dist->{provides} = $self->extract_packages($dist->{meta}, ".") if -e 'MANIFEST';
   
       # install direct 'test' dependencies for --installdeps, even with --notest
+      my $root_target = (($self->{installdeps} or $self->{showdeps}) and $depth == 0);
       $dist->{want_phases} = $self->{notest} && !$root_target
                            ? [qw( build runtime )] : [qw( build test runtime )];
   
@@ -3240,12 +3371,11 @@ $fatpacked{"App/cpanminus/script.pm"} = <<'APP_CPANMINUS_SCRIPT';
           return 1;
       };
   
-      require File::Find;
+      require ExtUtils::Manifest;
       require App::cpanminus::ParsePM;
   
-      my @files;
-      my $wanted = sub { s/^\Q$dir\E\///; push @files, $_ if /\.pm/ && $try->($_) };
-      File::Find::find({ wanted => $wanted, no_chdir => 1 }, $dir);
+      my $manifest = eval { ExtUtils::Manifest::manifind() } || {};
+      my @files = grep { /\.pm$/ && $try->($_) } keys %$manifest;
   
       my $provides = {};
   
